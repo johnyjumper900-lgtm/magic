3f140e4192a3b53efb9d4f9d6bb2a2a905f40d04
johnyjumper900-lgtm <johnyjumper900@gmail.com>
2026-04-28 21:58:59 +0200

    Fix: Final attempt for page blanche and index error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.setenza.magic',
   appName: 'magic',
-  webDir: 'dist/client' // <--- Changement ici pour correspondre à la sortie de Vite
+  webDir: 'dist', // On revient à dist pour plus de flexibilité
+  bundledWebRuntime: false
 };
 
 export default config;
